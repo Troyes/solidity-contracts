c7542cd7de2490ddd86ccb51353ff22a46a6b5a3
Eduardo Duran <zakkred1@gmail.com>
2018-05-24 00:35:29 -0500

    StaxeToken: fixed tests

--- test/StaxeToken.test.js
+++ test/StaxeToken.test.js
@@ -1,8 +1,7 @@
 
 const StaxeToken = artifacts.require("./StaxeToken.sol");
-const secondAccount = '0x02';
 const decimals = 18;
-const initialAmount = 10000;
+const initialAmount = 10000000;
 
 contract('StaxeToken', (accounts) => {
 
@@ -12,16 +11,16 @@ contract('StaxeToken', (accounts) => {
             let balance = await token.balanceOf.call(accounts[0]);
 
             let expected = initialAmount * (10 ** decimals);
-            assert.equal(balance.valueOf(), expected, "We should have 10000 Staxe tokens in first account.")
+            assert.equal(balance.valueOf(), expected, "We should have 10000000 Staxe tokens in first account.")
         });
     });
 
     describe('mint', async () => {
         it("should be able to mint 1007 Staxe tokens", async () => {
             let token = await StaxeToken.deployed();
-            let mint = await token.mint(secondAccount, 1007);
+            let mint = await token.mint(accounts[1], 1007);
 
-            let expectedBalance = await token.balanceOf.call(secondAccount);
+            let expectedBalance = await token.balanceOf.call(accounts[1]);
             assert.equal(expectedBalance, 1007, "We should have 1007 Staxe tokens on second account.");
         });
     });
